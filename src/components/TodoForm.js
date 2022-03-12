585c1c34d74b000ba463f77b8da525946342f342
Jake Opie <jakeopie9@gmail.com>
2022-03-11 20:23:26 -0600

    task add noise added

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,6 +1,10 @@
 import React, {useState} from 'react';
 import { v4 } from "uuid";
 import { Button, TextField } from "@material-ui/core";
+import UIfx from 'uifx';
+import menuAdd from '../my-sounds/menuAdd.mp3';
+
+const menuSound = new UIfx(menuAdd);
 
 function TodoForm({ addTodo }) {
     const [todo, setTodo] = useState({
@@ -15,6 +19,7 @@ function TodoForm({ addTodo }) {
 
     function handleSubmit(e) {
         e.preventDefault();
+        menuSound.play(0.3);
         if (todo.task.trim()) {
             addTodo({ ...todo, id: v4() });
             setTodo({ ...todo, task: "" });
